2e56f9b1a3b51743a6d337c704bb827cf32a5c72
jinyongp <dev.jinyongp@gmail.com>
2022-03-15 01:36:05 +0900

    Style: 검색창 스타일 적용

--- src/components/search-bar.vue
+++ src/components/search-bar.vue
@@ -1,25 +1,27 @@
 <template>
-  <div>
-    <span>검색 결과 ＞</span>
-    <span>{{ query }}</span>
-  </div>
-  <input
-    type="text"
-    v-model.trim="text"
-    placeholder="기업명을 검색하세요."
-    @input="change"
-    @focus="focus"
-    @blur="blur"
-    @keypress.enter="submit"
-  />
-  <div v-if="focused">
-    <ul v-if="matched">
-      <li v-for="(suggestion, index) in suggestions" :key="index">
-        {{ suggestion }}
-      </li>
-    </ul>
-    <span v-else>해당 기업 정보가 없습니다.</span>
-  </div>
+  <section>
+    <div class="row">
+      <span>검색 결과</span>
+      <span>{{ query }}</span>
+    </div>
+    <input
+      type="text"
+      v-model.trim="text"
+      placeholder="기업명을 검색하세요."
+      @input="change"
+      @focus="focus"
+      @blur="blur"
+      @keypress.enter="submit"
+    />
+    <div class="suggestions" v-if="(focused && suggestions.length) || !matched">
+      <ul v-if="matched">
+        <li v-for="(suggestion, index) in suggestions" :key="index">
+          {{ suggestion }}
+        </li>
+      </ul>
+      <span v-else>해당 기업 정보가 없습니다.</span>
+    </div>
+  </section>
 </template>
 
 <script>
@@ -68,3 +70,65 @@ export default {
   },
 };
 </script>
+
+<style lang="scss">
+section {
+  display: flex;
+  flex-direction: column;
+  padding: 16px;
+  gap: 20px;
+  position: relative;
+
+  .row {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    color: #727272;
+
+    span:first-child {
+      font-size: 16px;
+      font-weight: bold;
+    }
+
+    span:last-child {
+      font-size: 14px;
+    }
+  }
+
+  input {
+    padding: 14px 16px;
+    font-size: 14px;
+    border: 1px solid #f2f2f2;
+    background-color: #f8f8f8;
+    border-radius: 4px;
+    outline: none;
+
+    &::placeholder {
+      color: #b2b2b2;
+    }
+  }
+
+  .suggestions {
+    position: absolute;
+    box-sizing: border-box;
+    max-height: 200px;
+    overflow-y: auto;
+    top: 100px;
+    left: 16px;
+    right: 16px;
+    padding: 8px;
+    border: 1px solid #f2f2f2;
+    border-radius: 4px;
+
+    li {
+      border: 4px;
+      padding: 8px;
+      cursor: pointer;
+
+      &:hover {
+        background-color: rgb(0 0 0 / 5%);
+      }
+    }
+  }
+}
+</style>
