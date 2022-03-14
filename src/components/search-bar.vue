2de0f5a1d62cca9e9fa2e7503c6550255e98a9ba
jinyongp <dev.jinyongp@gmail.com>
2022-03-14 19:47:50 +0900

    Update: 검색창 변경
    - getSuggestions 함수 분리
    - 검색어 입력 후 엔터 누르면 첫번째 추천어로 자동완성

--- src/components/search-bar.vue
+++ src/components/search-bar.vue
@@ -4,6 +4,8 @@
     <span>{{ query }}</span>
   </div>
   <input
+    type="text"
+    v-model="text"
     placeholder="기업명을 검색하세요."
     @input="change"
     @blur="blur"
@@ -19,28 +21,36 @@
 <script>
 import enterprises from "../data/enterprise.json";
 
+function getSuggestions(keyword) {
+  const query = new RegExp(keyword.split("").join(".*?"), "gi");
+  return enterprises
+    .map(({ enterprise }) => enterprise)
+    .filter((enterprise) => query.test(enterprise));
+}
+
 export default {
   name: "SearchBar",
   data() {
     return {
+      text: "",
       query: "",
       suggestions: [],
+      showList: false,
     };
   },
   methods: {
     change({ target: { value } }) {
-      const query = new RegExp(value.split("").join(".*?"), "gi");
-      this.suggestions = value
-        ? enterprises
-            .map(({ enterprise }) => enterprise)
-            .filter((enterprise) => query.test(enterprise))
-        : [];
+      this.suggestions = value ? getSuggestions(value) : [];
     },
     blur() {
       this.suggestions = [];
     },
-    submit({ target: { value } }) {
-      this.query = value;
+    submit() {
+      const suggestion = this.suggestions[0];
+      if (suggestion) {
+        this.query = suggestion;
+        this.text = suggestion;
+      }
     },
   },
 };
