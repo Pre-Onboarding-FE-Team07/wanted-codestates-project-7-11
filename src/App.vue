5ef3ff97ca3aac5dc30a41edc0a2473895133b2a
dev-seomoon <skm1104@daum.net>
2022-03-15 20:35:56 +0900

    Feat: App.vue에 Pentagon chart 연결

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <search-bar-vue />
+  <chart-pentagon-vue :user-result="userData" :enterprise="enterpriseData[0]" />
   <chart-bar-view-vue :user="user" :company="company" />
 </template>
 
@@ -8,17 +9,21 @@ import { computed } from "vue";
 import userData from "./datas/user.json";
 import enterpriseData from "./datas/enterprise.json";
 import SearchBarVue from "./components/SearchBar.vue";
+import ChartPentagonVue from "./components/ChartPentagon.vue";
 import ChartBarViewVue from "./components/ChartBarView.vue";
 
 export default {
   components: {
     SearchBarVue,
+    ChartPentagonVue,
     ChartBarViewVue,
   },
   data() {
     return {
       user: computed(() => Object.values(userData)),
+      userData: userData,
       company: computed(() => Object.values(enterpriseData[0].result)),
+      enterpriseData: enterpriseData,
     };
   },
 };
