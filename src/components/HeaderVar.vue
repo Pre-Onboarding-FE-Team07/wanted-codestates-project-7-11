8e682973169284a22465a7d4da289767d5b8dcb0
Lela12 <dbcocotn4@hanmail.net>
2022-03-15 20:52:30 +0900

    Feat: header,tab 구현

---
+++ src/components/HeaderVar.vue
@@ -0,0 +1,74 @@
+<template>
+  <div class="header">
+    <div class="title-wrap">
+      <div class="arrow-wrap">
+        <img src="../assets/back-button.png" />
+      </div>
+      <h1 class="title">진단결과</h1>
+    </div>
+    <p class="reload" @click="reload">다시 진단하기</p>
+  </div>
+</template>
+
+<script>
+export default {
+  method: {
+    reload() {
+      window.location.reload();
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.header {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  position: absolute;
+  width: 360px;
+  height: 72px;
+  padding: 16px;
+  top: 0;
+  background: #ffffff;
+  border: 1px solid lightgray;
+}
+
+.title-wrap {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  width: 220px;
+  height: 34px;
+}
+
+.title {
+  width: 100px;
+  height: 34px;
+  font-weight: 700;
+  font-size: 24px;
+  line-height: 140%;
+  color: #121212;
+  margin: 0px 12px;
+}
+
+.arrow-wrap {
+  margin: 5px;
+}
+
+img {
+  width: 28px;
+  height: 28px;
+}
+
+.reload {
+  font-size: 16px;
+  font-weight: 400;
+  line-height: 22px;
+  text-align: right;
+  font-style: normal;
+  color: #6e3cf9;
+  cursor: pointer;
+}
+</style>
---
+++ src/components/TabsSelect.vue
@@ -0,0 +1,110 @@
+<template>
+  <section>
+    <div class="tab-container">
+      <button
+        v-for="(item, index) in tabItems"
+        @click="onEmit(index)"
+        :key="index"
+        :class="{ active: selectedTab === index }"
+      >
+        <img :src="item.img" />
+        <span class="tab-title">{{ item.title }}</span>
+      </button>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  components: {},
+
+  emits: ["tabClick"],
+  methods: {
+    onEmit(id) {
+      this.selectedTab = id;
+      this.$emit("tabClick", this.selectedTab);
+    },
+  },
+  mounted() {},
+
+  data() {
+    return {
+      selectedTab: 0,
+      tabItems: [
+        {
+          title: "모두",
+          img: require("../assets/all.png"),
+        },
+        {
+          title: "본인",
+          img: require("../assets/me.png"),
+        },
+        {
+          title: "회사",
+          img: require("../assets/company.png"),
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+section {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 20px 16px;
+  width: 360px;
+  height: 84px;
+  top: 381px;
+  background: #ffffff;
+}
+
+.tab-container {
+  display: flex;
+  flex-direction: row;
+  align-items: flex-start;
+  width: 328px;
+  height: 45px;
+  left: 16px;
+  top: 20px;
+  background: #f8f8f8;
+  border: 1px solid #f2f2f2;
+  box-sizing: border-box;
+}
+
+button {
+  display: flex;
+  width: 109px;
+  height: 45px;
+  border-radius: 4px;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  padding: 14px 16px 14px 16px;
+  background: #f2f2f2;
+  border: none;
+  flex-grow: 1;
+}
+
+img {
+  width: 14px;
+}
+
+button.active {
+  background-color: white;
+  color: #121212;
+}
+
+.tab-title {
+  width: 30px;
+  height: 17px;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 12px;
+  line-height: 17px;
+  order: 1;
+  margin: 0 6px;
+}
+</style>
