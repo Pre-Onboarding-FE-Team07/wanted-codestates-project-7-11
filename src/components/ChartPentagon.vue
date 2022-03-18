8a6b6a99aa0ce88b41b3d5f856f2a9d936120b44
dev-seomoon <skm1104@daum.net>
2022-03-18 12:00:03 +0900

    Feat: Pentagon Chart에 기업 검색결과 연결

--- src/components/ChartPentagon.vue
+++ src/components/ChartPentagon.vue
@@ -6,7 +6,7 @@
 </template>
 
 <script>
-import { defineComponent, reactive } from "vue";
+import { defineComponent, watch, ref } from "vue";
 import { RadarChart } from "vue-chart-3";
 import { Chart, registerables } from "chart.js";
 
@@ -60,53 +60,64 @@ export default defineComponent({
         },
       },
     };
-    const chartData = reactive({
-      labels: [
-        ["적극적인", "Aggressive"],
-        ["자신있는", "Confident"],
-        ["책임있는", "Responsible"],
-        ["개인주의", "Indivisual"],
-        ["수평적인", "Horizontal"],
-      ],
-      datasets: [
-        {
-          data: props.userResult,
-          backgroundColor: "rgba(110, 60, 249, 0.32)",
-          borderColor: "rgba(110, 60, 249, 1)",
-          borderWidth: 2,
-          borderJoinStyle: "round",
-          pointRadius: 0,
-          order: 1,
-        },
-        {
-          data: props.enterpriseResult || null,
-          backgroundColor: "rgba(255, 193, 74, 0.32)",
-          borderColor: "rgba(255, 211, 53, 1)",
-          borderWidth: 2,
-          borderJoinStyle: "round",
-          pointRadius: 0,
-          order: 0,
-        },
-        {
-          data: [10, 10, 10, 10, 10],
-          pointRadius: 16,
-          borderWidth: 1,
-          backgroundColor: "rgba(238, 238, 238, 0.32)",
-          borderColor: "rgba(178, 178, 178, 1)",
-          pointHoverRadius: 16,
-          pointBackgroundColor: [
-            "rgba(235, 133, 108, 0.7)",
-            "rgba(108, 135, 245, 0.5)",
-            "rgba(64, 171, 199, 0.5)",
-            "rgba(229, 115, 160, 0.5)",
-            "rgba(101, 184, 81, 0.5)",
-          ],
-          order: 2,
-        },
-      ],
-    });
+    const chartData = ref(
+      createChart(props.userResult, props.enterpriseResult)
+    );
+
+    watch(
+      () => props.enterpriseResult,
+      () => {
+        chartData.value = createChart(props.userResult, props.enterpriseResult);
+      }
+    );
     return { options, chartData };
   },
+});
+
+const createChart = (userResult, enterpriseResult) => ({
+  labels: [
+    ["적극적인", "Aggressive"],
+    ["자신있는", "Confident"],
+    ["책임있는", "Responsible"],
+    ["개인주의", "Indivisual"],
+    ["수평적인", "Horizontal"],
+  ],
+  datasets: [
+    {
+      data: userResult,
+      backgroundColor: "rgba(110, 60, 249, 0.32)",
+      borderColor: "rgba(110, 60, 249, 1)",
+      borderWidth: 2,
+      borderJoinStyle: "round",
+      pointRadius: 0,
+      order: 1,
+    },
+    {
+      data: enterpriseResult || null,
+      backgroundColor: "rgba(255, 193, 74, 0.32)",
+      borderColor: "rgba(255, 211, 53, 1)",
+      borderWidth: 2,
+      borderJoinStyle: "round",
+      pointRadius: 0,
+      order: 0,
+    },
+    {
+      data: [10, 10, 10, 10, 10],
+      pointRadius: 16,
+      borderWidth: 1,
+      backgroundColor: "rgba(238, 238, 238, 0.32)",
+      borderColor: "rgba(178, 178, 178, 1)",
+      pointHoverRadius: 16,
+      pointBackgroundColor: [
+        "rgba(235, 133, 108, 0.7)",
+        "rgba(108, 135, 245, 0.5)",
+        "rgba(64, 171, 199, 0.5)",
+        "rgba(229, 115, 160, 0.5)",
+        "rgba(101, 184, 81, 0.5)",
+      ],
+      order: 2,
+    },
+  ],
 });
 </script>
 
